8c95da98f6bd68dd544bf9aa136f964ab3824b25
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-03-26 00:22:00 +0200

    Fix Storybook issues

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,21 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  webpackFinal: (config) => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        // Storybook will understand how handle storybook/** import alias
+        storybook: path.resolve(__dirname, "../src/storybook"),
+      },
+    },
+  }),
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+};
