1ef69d95da3d2ab2bc84e045689548a35e538e23
Oleksandr Hridin <alexander.m.gridin@gmail.com>
2023-03-27 21:57:27 +0300

    Configurations updated

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -12,7 +12,7 @@ import packageJson from "./package.json" assert { type: "json" };
 export default [
   {
     input: "src/index.ts",
-    external: ["react-dom"],
+    external: ["react", "react-dom", "react-router-dom"],
     output: [
       {
         file: packageJson.main,
